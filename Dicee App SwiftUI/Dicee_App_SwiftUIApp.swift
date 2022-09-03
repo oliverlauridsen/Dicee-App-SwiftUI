da93539e8b9380adc16ef7a51c681cce4b6f4af9
Oliver Lauridsen <olidude2.0@gmail.com>
2022-09-03 15:53:57 +0200

    Initial Commit

---
+++ Dicee App SwiftUI/Dicee_App_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_App_SwiftUIApp.swift
+//  Dicee App SwiftUI
+//
+//  Created by Oliver Lauridsen on 03/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_App_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
