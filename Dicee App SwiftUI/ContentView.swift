d9e0f0685ac8820c4caf289ef04f7b6a321c1476
Oliver Lauridsen <olidude2.0@gmail.com>
2022-09-04 08:52:28 +0200

    Add assets & make dice mutable with @State & add roll functionality

--- Dicee App SwiftUI/ContentView.swift
+++ Dicee App SwiftUI/ContentView.swift
@@ -8,14 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var leftDiceNumber = 1
+    @State var rightDiceNumber = 2
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Image("background")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Image("diceeLogo")
+                Spacer()
+                HStack {
+                    DiceView(n: leftDiceNumber)
+                    DiceView(n: rightDiceNumber)
+                }
+                Spacer()
+                Button(action: {
+                    self.leftDiceNumber = Int.random(in: 1...6)
+                    self.rightDiceNumber = Int.random(in: 1...6)
+
+                }) {
+                    Text("Roll")
+                        .font(.system(size:50))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                        .padding(.horizontal)
+                }
+                .background(Color.red)
+                .padding()
+               
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct DiceView: View {
+    var n: Int
+    var body: some View {
+        Image("dice\(n)")
+            .padding(.all)
     }
 }
